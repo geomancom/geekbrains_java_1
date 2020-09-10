c2325e3ff0a0b6fa3aad1601953002cd05db51aa
Manbob <54456534+geomancom@users.noreply.github.com>
2020-09-10 16:35:47 +0500

    Update Main.java

--- DZ_3/Main.java
+++ DZ_3/Main.java
@@ -20,20 +20,13 @@ public class Main {
         }
     }
 
+    
+    private static int start() {
+        System.out.println("\nВведите число от 0 до 9: ");
+        return (int) (Math.random() * RANGE);
+    }    
 
-
-    private static void doCycle() {
-        int user_number = in.nextInt();
-        if (user_number == number) {
-            System.out.printf("Вы угадали загаданное число = %d", number);
-            cycle = 3;
-        } else if (user_number > number) {
-            System.out.printf("Попробуйте еще раз, загаданное число меньше.\nУ Вас осталось еще %d попыток\n", 3 - cycle);
-        } else {
-            System.out.printf("Попробуйте еще раз, загаданное число больше.\nУ Вас осталось еще %d попыток\n", 3 - cycle);
-        }
-    }
-
+    
     private static void doQuestion() {
             System.out.println("\nПовторить игру еще раз? 1 – да / 0 – нет");
             int user_number_exit = in.nextInt();
@@ -47,12 +40,21 @@ public class Main {
                 default:
                     System.out.println("Выберите или 0 или 1, Повторить игру еще раз? 1 – да / 0 – нет\n");
             }
+    }    
+
+    
+    private static void doCycle() {
+        int user_number = in.nextInt();
+        if (user_number == number) {
+            System.out.printf("Вы угадали загаданное число = %d", number);
+            cycle = 3;
+        } else if (user_number > number) {
+            System.out.printf("Попробуйте еще раз, загаданное число меньше.\nУ Вас осталось еще %d попыток\n", 3 - cycle);
+        } else {
+            System.out.printf("Попробуйте еще раз, загаданное число больше.\nУ Вас осталось еще %d попыток\n", 3 - cycle);
+        }
     }
 
-    private static int start() {
-        System.out.println("\nВведите число от 0 до 9: ");
-        return (int) (Math.random() * RANGE);
-    }
 
     private static void doExit() {
         System.out.println("Вы точно хотите выйти? y/n");
